cede12f622076ab99b2ab4e4482cf2420cfbef24
ikumi-rai <tajima_ikumi@me.com>
2025-01-29 09:56:34 +0900

    微リファクタリング

--- dex-navigator/service_worker.js
+++ dex-navigator/service_worker.js
@@ -1,7 +1,4 @@
 import { setContextMenu, setContextMenuScript } from "./context_menu.js"
 
-chrome.runtime.onInstalled.addListener(async () => {
-  setContextMenu()
-})
-
+chrome.runtime.onInstalled.addListener(setContextMenu)
 setContextMenuScript()
